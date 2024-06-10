7aef3422a6404633e70ace6afcbbd2d711229066
Mayank Sethi <mayank@digibrain.co.in>
2024-06-10 14:42:05 +0530

    update value of player name after clicking save button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
   };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" placeholder={name} required />
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          />
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
