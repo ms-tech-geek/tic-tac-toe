f7836ba34b1fda70706280c4c190d19ba1b16503
Mayank Sethi <mayank@digibrain.co.in>
2024-06-23 04:30:31 +0530

    configure app for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // Use relative paths
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'TicTacToe',
+        name: 'Tic Tac Toe',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/png',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+    }),
+  ],
+  base: './',
   build: {
     rollupOptions: {
       output: {
@@ -13,4 +34,5 @@ export default defineConfig({
       },
     },
   },
+  logLevel: 'debug', // Add this line for more detailed logging
 });
