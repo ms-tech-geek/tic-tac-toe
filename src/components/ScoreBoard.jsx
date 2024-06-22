955820a1f03aa0e8bc0d2a06b4e3f9ee4fca7e46
Mayank Sethi <mayank@digibrain.co.in>
2024-06-22 15:05:30 +0530

    make scoreBoard as dynamic component

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,12 +1,11 @@
 const ScoreBoard = ({ players, scoreBoard }) => {
   return (
     <div id="scoreboard">
-      <span style={{ margin: '2rem', fontSize: '1.5rem' }}>
-        {players.X} : {scoreBoard.X} Wins
-      </span>
-      <span style={{ margin: '2rem', fontSize: '1.5rem' }}>
-        {players.O} : {scoreBoard.O} Wins
-      </span>
+      {Object.keys(players).map((playerKey) => (
+        <span key={playerKey} style={{ margin: '2rem', fontSize: '1.5rem' }}>
+          {players[playerKey]} : {scoreBoard[playerKey]} Wins
+        </span>
+      ))}
     </div>
   );
 };
