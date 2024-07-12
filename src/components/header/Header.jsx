eeeef07e6dd5672e798e65a15b3349c22ceb9509
Mayank Sethi <mayank@digibrain.co.in>
2024-07-13 04:42:26 +0530

    restyle header with tailwind

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
-import styles from './styles.module.scss';
-
 const Header = () => {
   return (
     <header>
-      <h1 className={styles.headerTitle}>Tic-Tac-Toe</h1>
+      <h1 className="text-4xl sm:text-3xl font-bold text-center font-caprasimo my-4 text-stone-700">
+        Tic-Tac-Toe
+      </h1>
     </header>
   );
 };
