ebd4df6e4a5211c0d2033f0f8aa6c431a2980323
Mayank Sethi <mayank@digibrain.co.in>
2024-07-12 17:53:52 +0530

    Bugfix - game mode button styles

--- src/components/gamemode/GameModeSelector.jsx
+++ src/components/gamemode/GameModeSelector.jsx
@@ -2,17 +2,28 @@ import styles from './styles.module.scss';
 
 const GameModeSelector = ({ onSelectMode }) => {
   return (
-    <div className={styles.gameModeContainer}>
-      <div className={styles.gameModeSelector}>
-        <h2>Select Game Mode</h2>
-        <button onClick={() => onSelectMode('pvp')} className={styles.button}>
-          Player vs Player
-        </button>
-        <button onClick={() => onSelectMode('pvc')} className={styles.button}>
-          Player vs Computer
-        </button>
+    <>
+      <div className={styles.gameModeContainer}>
+        <div className={styles.gameModeSelector}>
+          <h2>Select Game Mode</h2>
+          <div className={styles.buttonContainer}>
+            <button
+              onClick={() => onSelectMode('pvp')}
+              className={styles.button}
+            >
+              Player vs Player
+            </button>
+
+            <button
+              onClick={() => onSelectMode('pvc')}
+              className={styles.button}
+            >
+              Player vs Computer
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
