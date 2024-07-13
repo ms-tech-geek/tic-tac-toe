b53154fe240f7442932986eda16c345b4a3329f1
Mayank Sethi <mayank@digibrain.co.in>
2024-07-13 05:38:08 +0530

    refactor styles for game mode selector

--- src/components/gamemode/GameModeSelector.jsx
+++ src/components/gamemode/GameModeSelector.jsx
@@ -1,35 +1,21 @@
 const GameModeSelector = ({ onSelectMode }) => {
-  const buttonStyle = `
-    font-caprasimo
-    text-xl 
-    bg-[#4b4b4b] 
-    hover:bg-[#fcd256] 
-    text-[#fcd256] 
-    hover:text-[#3f3b00]
-    border-2 border-[#fcd256] 
-    p-2 
-    px-4 
-    rounded-[4px] 
-    cursor-pointer 
-    transition-all 
-    duration-200 
-    my-2 
-    flex-grow 
-    transform 
-    hover:scale-105
-  `;
-
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="max-w-[30rem] bg-stone-700 p-8 rounded-[6px] bg-gradient-to-b from-[#383624] to-[#282617] shadow-[0_0_20px_rgba(0,0,0,0.5)]">
-        <div className="flex flex-col items-center">
-          <h2 className="text-5xl mb-4 font-caprasimo">Game Mode</h2>
-          <div className="flex flex-col">
-            <button onClick={() => onSelectMode('pvp')} className={buttonStyle}>
+    <div className="gameModeContainerWrapper">
+      <div className="gameModeContainer">
+        <div className="gameModeContentWrapper">
+          <h2 className="gameModeHeader">Game Mode</h2>
+          <div className="gameModeButtonsContainer">
+            <button
+              onClick={() => onSelectMode('pvp')}
+              className="gameModeButtons"
+            >
               Player vs Player
             </button>
 
-            <button onClick={() => onSelectMode('pvc')} className={buttonStyle}>
+            <button
+              onClick={() => onSelectMode('pvc')}
+              className="gameModeButtons"
+            >
               Player vs Computer
             </button>
           </div>
