e05dac671cd70878804fad19ee0419b17282e488
ms-tech-geek <thecorporator@gmail.com>
2024-11-05 09:40:00 +0530

    configure app with redux provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { gameStore } from './store/gameStore';
 import './global.scss';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={gameStore}>
+    <App />
+  </Provider>
+);
